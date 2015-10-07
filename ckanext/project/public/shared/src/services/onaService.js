32b6d09449780527a036c9250e6325bce9a9d48b
Daniel Baah <dbaah@spatialdev.com>
2015-10-07 10:24:48 -0700

    ona service

--- ckanext/project/public/shared/src/services/onaService.js
+++ ckanext/project/public/shared/src/services/onaService.js
@@ -1,34 +1,45 @@
-//
-//
-//var app = angular.module("app")
-//    .service("onaService", ['$http', '$q', 'ENV', function ($http, $q, ENV) {
-//
-//        var service =  {};
-//
-//        /**
-//         * This function gets all of the data required for the parcel overview page
-//         * @returns {*}
-//         * todo pass in project id
-//         */
-//        service.submitSurvey = function(){
-//
-//            var deferred = $q.defer();
-//
-//            var opts = {
-//                "headers":'Authorization'
-//            }
-//
-//            $http.post('https://ona.io/api/v1/forms', { cache: false })
-//                .then(function(response) {
-//                    deferred.resolve(response.data);
-//                }, function(response) {
-//                    deferred.reject(response);
-//                });
-//
-//            return deferred.promise;
-//        };
-//
-//
-//        return service;
-//    }])
-//
+
+
+var app = angular.module("app")
+    .service("fieldDataService", ['$http', '$q', 'ENV', function ($http, $q, ENV) {
+
+        var service =  {};
+
+        service.getOnaForm = function(formid){
+
+            var options = {
+                "Authorization": "Token 74756f0ab0da149f649e9074c529b633f3daaa02"
+            };
+
+            $http.get('http://54.245.82.92/api/v1/forms/'+formid, options)
+                .then(function(response) {
+                    deferred.resolve(response.data);
+                }, function(response) {
+                    deferred.reject(response);
+                });
+
+            return deferred.promise;
+        };
+
+        /**
+         * //TODO use real project_id
+         * This function submits an ONA form to the Cadasta DB
+         * @returns {*} success response
+         *
+         */
+        service.submitForm = function(form, cb){
+
+
+            //TODO get real project id
+            $http.post(ENV.apiRoot + '/providers/ona/load-form/1', form)
+                .then(function(response) {
+                    cb(response.data);
+                }, function(response) {
+                    //TODO remove from ONA on error
+                    cb(response.error);
+                });
+        };
+
+        return service;
+    }]);
+
