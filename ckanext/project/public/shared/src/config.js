a15e75e52cba9dc4c51622c689c3f39030e2e3f9
Sarah Bindman <sbindman@spatialdev.com>
2015-10-13 10:46:26 -0700

    Updates to make user page look better

--- ckanext/project/public/shared/src/config.js
+++ ckanext/project/public/shared/src/config.js
@@ -1,5 +1,5 @@
 angular.module('app.config', [])
 
-.constant('ENV', {name:'development',apiCadastaRoot:'http://localhost:5001',apiCKANRoot:'http://localhost:5000/api/3/action'})
+.constant('ENV', {name:'staging',apiCadastaRoot:'http://54.69.121.180:3000',apiCKANRoot:'http://localhost:5000/api/3/action'})
 
 ;
