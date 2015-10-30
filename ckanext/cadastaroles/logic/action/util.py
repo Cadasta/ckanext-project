c0b9954a8879ea7ec5ab18e00155cd5f2976d743
gcorradini <gregcorradini@gmail.com>
2015-10-30 13:11:36 -0700

    [#68]: ona_api_token has to be unique but is not required

--- ckanext/cadastaroles/logic/action/util.py
+++ ckanext/cadastaroles/logic/action/util.py
@@ -38,7 +38,7 @@ def call_api(endpoint, function, **kwargs):
             # https://github.com/Cadasta/ckanext-project/issues/68
             #
             if 'violates unique constraint "project_ona_api_key_key"' in message:
-                error_dict = { 'message': ['ona_api_token already in use']}
+                error_dict = { 'message': ['Ona API Token already in use']}
 
             #
             # ckan/logic/__init__.error_summary.summarise seems to require a certain format
@@ -65,7 +65,7 @@ def cadasta_get_api(endpoint, params, _=None, **kwargs):
 
 
 def cadasta_post_api(endpoint, data, _=None, **kwargs):
-    return call_api(endpoint, requests.post, data=data, **kwargs)
+    return call_api(endpoint, requests.post, json=data, **kwargs)
 
 def cadasta_patch_api(endpoint, data, _=None, **kwargs):
     return call_api(endpoint, requests.patch, data=data, **kwargs)
